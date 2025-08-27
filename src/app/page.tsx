d1503306239651873eac4acb11a2cbadedc74487
itsabubakar <seeker99157@gmail.com>
2025-08-27 08:21:19 +0100

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen grid place-items-center p-8 sm:p-20 font-sans">
+      <main className="text-center">
+        <h1 className="text-3xl sm:text-4xl font-semibold tracking-tight">
+          Welcome to Sadiq&rsquo;s corner of the internet
+        </h1>
+      </main>
+    </div>
+  );
+}
