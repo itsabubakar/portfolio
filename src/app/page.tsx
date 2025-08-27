7328ef2657aab6c5f3a0d5781065d06b99565e44
itsabubakar <seeker99157@gmail.com>
2025-08-27 09:05:17 +0100

    Add deployment workflow, update .gitignore, and create new pages
    - Implement GitHub Actions workflow for deploying to VPS
    - Update .gitignore to include SSH keys
    - Add image for homepage and link to writings page
    - Create new writings page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="min-h-screen grid place-items-center p-8 sm:p-20 font-sans">
       <main className="text-center">
         <h1 className="text-3xl sm:text-4xl font-semibold tracking-tight">
           Welcome to Sadiq&rsquo;s corner of the internet
+          <Image width={100} height={100} src="/room-1.jpg" alt="Room 1" />
         </h1>
+        <Link href="/writings">Read my writings</Link>
       </main>
     </div>
   );
---
+++ src/app/writings/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste iusto
+      asperiores amet praesentium, similique eligendi quos in enim, non debitis
+      voluptatum temporibus laudantium modi illo excepturi numquam voluptas
+      adipisci magnam?
+    </div>
+  );
+};
+
+export default page;
